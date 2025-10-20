64d0bae2f7fe392398637418b96015914d9c8656
mohitrana9137-arch <mohitrana9137@gmail.com>
2025-10-20 19:52:14 +0530

    Update API credentials and session variables

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ if os.path.exists(".env"):
     load_dotenv(".env")
     
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "!")
+API_ID = int(os.getenv("API_ID", "20445864"))
+API_HASH = os.getenv("API_HASH", "aef4167a97c855cf4775935fa35d0922")
+SESSION = os.getenv("BQE3-qgAWm6Mh-d5-f5lX7BzJp_ywYMEgQTDaWcatP_ySxQMo3AJvYlehqAxbVzzf9pdl5CuSsIuxS-eH4PCC5XKkHXIbopTU8wcsvuqxnJzh0kI7BaRtkgmKqZGbRdF0wlwFrznnnnMLI8JVIhGqK1CYNazXo46Kr4zIumZ4c_vB5U0svsUHUbRRM2u2vk6UdFYe2Fa9IIAIPnk8cHzi1P-UZ8AVXvjeHscrCSyDbxOknfiBedez8SAVNisuYK6_OgszQMdTQSpQYU2bi03jbSf9lWFUMmfZ7HXXht5bL6hj5Lfc8GfkHiii6zUeqrg-KL_lJDItSpshx9m-TT-G6EgjzJrDwAAAAHp5VvlAQ")
+HNDLR = os.getenv("HNDLR", "8308121662")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
 
